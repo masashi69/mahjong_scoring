c82354b69098e6d9733186b66d75e84bd0183c90
masashi suzuki <smasa619@gmail.com>
2025-01-04 20:26:59 +0900

    Created calculate score function

--- summary.py
+++ summary.py
@@ -7,13 +7,12 @@ def CalcDeviationRank(rank):
     # To calcurate deviation of rank
     return abs(4 - rank)
 
-def main():
-    mj_df = pd.read_csv(FileName)
-    average_score = mj_df['score'].mean()
-    average_rank = mj_df['rank'].mean()
+def CalculateScore(scorefile):
+    average_score = scorefile['score'].mean()
+    average_rank = scorefile['rank'].mean()
     average_rank = CalcDeviationRank(average_rank)
 
-    for ply in mj_df.groupby('player'):
+    for ply in scorefile.groupby('player'):
         p_avg_score, p_avg_rank = ply[1][['score', 'rank']].mean()
         p_max = ply[1]['score'].max()
 
@@ -21,8 +20,8 @@ def main():
         p_avg_rank_abs = CalcDeviationRank(p_avg_rank)
 
         # T-score
-        TScore_s = (p_avg_score - average_score) / mj_df['score'].std() * 10 + 50
-        TScore_r = (p_avg_rank_abs - average_rank) / mj_df['rank'].std() * 10 + 50
+        TScore_s = (p_avg_score - average_score) /scorefile['score'].std() * 10 + 50
+        TScore_r = (p_avg_rank_abs - average_rank) / scorefile['rank'].std() * 10 + 50
         TScore = (TScore_s + TScore_r) / 2
 
         # Round
@@ -37,6 +36,12 @@ def main():
         print(f'    最高スコア: {p_max}')
         print(f'    平均順位  : {p_rank}')
         print(f'    スコア&ランク偏差値: {TScore}')
+
+
+def main():
+    mj_df = pd.read_csv(FileName)
+
+    CalculateScore(mj_df)
 
 if __name__ == '__main__':
     main()
